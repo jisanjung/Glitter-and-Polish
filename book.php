2edb63ee01d5914773e9a5535a0c866995c70978
Jisan Jung <jjjung13@gmail.com>
2019-03-29 16:21:52 -0400

    added php

--- book.php
+++ book.php
@@ -1,3 +1,32 @@
+<?php
+    $valid = "";
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $individual_or_group = $_POST['individual-or-group'];
+        $people = $_POST['people'];
+        $date = $_POST['date'];
+        $msg = $_POST['message'];
+        
+        if (empty($name) && empty($email) && empty($msg) && empty($individual_or_group)) {
+            $valid = "please enter all fields";
+        } else {
+            $valid = "passed";
+        }
+        
+        $to = 'jjsk8er13@gmail.com';
+        $subject = 'APPOINTMENT FOR '.$name;
+        $message = "Name: ".$name."\n"."Email: ".$email."\n"."Phone Number: ".$phone."\n"."Individual or Group: ".$individual_or_group."\n"."Number of People: ".$people."\n"."Date: ".$date."\n"."Message: "."\n".$msg;
+        $headers = "From: ".$email;
+        
+        if (mail($to, $subject, $message, $headers)) {
+            $valid = "thank you";
+        } else {
+            $valid = "failed";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -28,7 +57,7 @@
                 <ul>
                     <li><a href="index.html">Home</a></li>
                     <li><a href="service.html">Services</a></li>
-                    <li><a href="book.html">Book Online</a></li>
+                    <li><a href="book.php">Book Online</a></li>
                     <li><a href="#">Contact Us</a></li>
                 </ul>
                 <img src="images/iconfinder_menu_3671806.png" alt="menu" id="menu">
@@ -39,12 +68,29 @@
                     <ul>
                         <li><a href="index.html">Home</a></li>
                         <li><a href="service.html">Services</a></li>
-                        <li><a href="book.html">Book Online</a></li>
+                        <li><a href="book.php">Book Online</a></li>
                         <li><a href="#">Contact Us</a></li>
                     </ul>
                 </div>
             </div>
-            
+            <?php if($valid != ""): ?>
+                <div class="success"><?php echo $valid; ?></div>
+            <?php endif; ?>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" class="main-form" id="book-form" name="form" method="post">
+                <input type="text" name="name" id="name" placeholder="Name *">
+                <input type="email" name="email" id="name" placeholder="Email *">
+                <input type="phone" name="phone" id="phone-number" placeholder="Phone No. *">
+                    <br>
+                <input type="radio" name="individual-or-group" id="individual" value="individual">
+                <label>Individual *</label>
+                <input type="radio" name="individual-or-group" id="group" value="group">
+                <label>Group *</label>
+                <input type="number" name="people" id="people" placeholder="Number of People *">
+                <input type="datetime" name="date" id="date" placeholder="Date/Time *">
+                <textarea name="message" id="message" placeholder="Types of Services *"></textarea>
+                    <br>
+                <button type="submit" name="submit" id="submit">Submit</button>
+            </form>
             <footer id="services-footer" class="blur">
                 <h1>
                 THANK YOU
