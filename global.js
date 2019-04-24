d9529bfe3d5d219489123f3dce0c71af1a7774b0
Jisan Jung <jjjung13@gmail.com>
2019-04-23 21:53:51 -0400

    fixed toggle glitch

--- global.js
+++ global.js
@@ -7,22 +7,16 @@ var blur = document.getElementsByClassName("blur");
 function showMenu() {
     toggleNav.style.transform = "translateX(-100%)";
     
-    for(var i = 0; i < blur.length; i++) {
-        blur[i].style.filter = "blur(6px)";
-    }
+//    for(var i = 0; i < blur.length; i++) {
+//        blur[i].style.filter = "blur(6px)";
+//    }
 }
 function hideMenu() {
     toggleNav.style.transform = "translateX(100%)";
     
-    for(var i = 0; i < blur.length; i++) {
-        blur[i].style.filter = "blur(0)";
-    }
-}
-function clickOut(event) {
-    if (event.target != toggleNav && event.target.parentNode != toggleNav) {
-            hideMenu();
-        }
+//    for(var i = 0; i < blur.length; i++) {
+//        blur[i].style.filter = "blur(0)";
+//    }
 }
 hamburger.addEventListener("click", showMenu);
 x.addEventListener("click", hideMenu);
-window.addEventListener("mouseup", clickOut);
