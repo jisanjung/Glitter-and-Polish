3b042ea7f7e4bb7af0f823abf75f27e7dbe72206
Jisan Jung <jjjung13@gmail.com>
2019-12-16 23:42:31 -0500

    redesign/develop

--- map.js
+++
@@ -1,9 +0,0 @@
-var map = L.map('map').setView([40.0423, -75.4819], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([40.0423, -75.4819]).addTo(map)
-    .bindPopup("39 W Lancaster Avenue <br> Paoli, PA 19301")
-    .openPopup();
