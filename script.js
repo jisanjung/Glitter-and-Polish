a985a63b40b66854695b89bf828a468ddadf9613
Jisan Jung <jjjung13@gmail.com>
2019-12-17 17:45:50 -0500

    header scroll feature

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+    //---------start----------
     
+    // carousel
     $(".fade").slick({
         inifnite: true,
         speed: 500,
@@ -8,4 +10,17 @@ $(document).ready(function() {
         autoplay: true,
         autoplaySpeed: 5000
     });
+
+    // toggle header style on scroll
+    $(window).on("scroll", function(e) {
+        var top = $(this).scrollTop();
+        
+        if (top > 0) {
+            $("header").css({"top": "0", "background": "#000"});
+        } else {
+            $("header").css({"top": "20px", "background": "transparent"});
+        }
+    });
+
+    //----------end------------
 });
