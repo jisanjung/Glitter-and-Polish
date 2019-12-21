024eb851ffa653c24edba306f4491059352198e8
Jisan Jung <jjjung13@gmail.com>
2019-12-21 14:16:26 -0500

    smooth scrolling button

--- script.js
+++ script.js
@@ -17,16 +17,30 @@ $(document).ready(function() {
     $(window).on("scroll", function(e) {
         var top = $(this).scrollTop();
         
+        // header
         if (top > 0) {
             $("header").css({"top": "0", "background": "#000"});
         } else {
             $("header").css({"top": "20px", "background": "transparent"});
+        }
+
+        // reveal button takes you back to top
+        if (top > 1000) {
+            $(".back-to-top").css("right", "0px");
+        } else {
+            $(".back-to-top").css("right", "-50px");
         }
     });
 
     // toggle nav menu
     $(".menu").on("click", function() {
         $("nav").slideToggle();
+    });
+
+    // smooth scroll
+    $('a[href*=\\#]').on('click', function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
     });
 
     //----------end------------
