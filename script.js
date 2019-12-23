81beaa8a3fe5c21755aa88ac7cda139b7fcc19b6
Jisan Jung <jjjung13@gmail.com>
2019-12-23 02:37:03 -0500

    basic modal feature

--- script.js
+++ script.js
@@ -43,5 +43,49 @@ $(document).ready(function() {
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
     });
 
+    // leaflet map
+    (function setMap() {
+        var mymap = L.map('mapid').setView([40.0423, -75.4819], 13);
+        
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/           copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+        var marker = L.marker([40.0423, -75.4819]).addTo(mymap);
+        marker.bindPopup("39 W Lancaster Avenue <br> Paoli, PA 19301").openPopup();
+    })();
+
+    // modal
+    (function modalFunc() {
+        var modal = {
+            image: $("#image"),
+            background: $("#modalBackground"),
+            selectImage: function() {
+                var modalImage = modal.image;
+
+                this.background.css("display", "block");
+                this.image.css("display", "block");
+
+            },
+            clickOut: function() {
+                this.background.css("display", "none");
+                this.image.css("display", "none");
+            }
+        };
+
+        $(".img").each(function(i) {
+            $(this).on("click", function() {
+                // changes the "this" reference to modal object
+                // shows clicked image
+                modal.selectImage.call(modal);
+
+                // attaches clicked img src to the modal's src
+                modal.image.attr("src", this.src);
+            });
+        });
+        modal.background.on("click", function() {
+            modal.clickOut.call(modal);
+        });
+    })();
+
     //----------end------------
 });
