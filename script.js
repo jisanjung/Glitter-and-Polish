0d53f1f2425d1d1dbe6ab3b7d5b9b71bfc8fb99a
Jisan Jung <jjjung13@gmail.com>
2019-12-23 13:04:39 -0500

    form validation

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ $(document).ready(function() {
         marker.bindPopup("39 W Lancaster Avenue <br> Paoli, PA 19301").openPopup();
     })();
 
+
     // modal
     (function modalFunc() {
         var modal = {
@@ -85,6 +86,94 @@ $(document).ready(function() {
         modal.background.on("click", function() {
             modal.clickOut.call(modal);
         });
+    })();
+
+
+    // form validation
+    (function formValid() {
+        var isValid = {
+            name: function() {
+                var name = $("#name");
+
+                name.on("blur", function() {
+                    if (!/^[a-z ,.'-]+$/i.test(name.val())) {
+                        $(this).css("border", "1px solid red");
+                        $("#nameError").show();
+                    } else {
+                        $(this).css("border", "1px solid #ccc");
+                        $("#nameError").hide();
+                    }
+                });
+                name.on("focus", function() {
+                    if (/^[a-z ,.'-]+$/i.test(name.val())) {
+                        $(this).css("border", "1px solid #ccc");
+                        $("#nameError").hide();
+                    }
+                });
+            },
+            email: function() {
+                var email = $("#email");
+
+                email.on("blur", function() {
+                    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.val())) {
+                        $(this).css("border", "1px solid red");
+                        $("#emailError").show();
+                    } else {
+                        $(this).css("border", "1px solid #ccc");
+                        $("#emailError").hide();
+                    }
+                });
+                email.on("focus", function() {
+                    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.val())) {
+                        $(this).css("border", "1px solid #ccc");
+                        $("#emailError").hide();
+                    }
+                });
+            },
+            subject: function() {
+                var subject = $("#subject");
+
+                subject.on("blur", function() {
+                    if (subject.val() === "" || subject.val() === null || subject.val() === undefined) {
+                        $(this).css("border", "1px solid red");
+                        $("#subjectError").show();
+                    } else {
+                        $(this).css("border", "1px solid #ccc");
+                        $("#subjectError").hide();
+                    }
+                });
+                subject.on("focus", function() {
+                    if (subject.val() !== "" || subject.val() !== null || subject.val() !== undefined) {
+                        $(this).css("border", "1px solid #ccc");
+                        $("#subjectError").hide();
+                    }
+                });
+            },
+            message: function() {
+                var message = $("#message");
+
+                message.on("blur", function() {
+                    if (message.val() === "" || message.val() === null || message.val() === undefined) {
+                        $(this).css("border", "1px solid red");
+                        $("#messageError").show();
+                    } else {
+                        $(this).css("border", "1px solid #ccc");
+                        $("#messageError").hide();
+                    }
+                });
+                message.on("focus", function() {
+                    if (message.val() !== "" || message.val() !== null || message.val() !== undefined) {
+                        $(this).css("border", "1px solid #ccc");
+                        $("#messageError").hide();
+                    }
+                });
+            }
+        };
+
+        isValid.name();
+        isValid.email();
+        isValid.subject();
+        isValid.message();
     })();
 
     //----------end------------
