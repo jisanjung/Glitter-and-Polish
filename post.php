cfa853eed312ef7b7a6636d383067d52f7d30264
Jisan Jung <jjjung13@gmail.com>
2023-05-15 11:37:36 -0400

    feature: send appointments thru email

--- post.php
+++ post.php
@@ -1,30 +1,41 @@
 <?php
-    $conn = new mysqli("localhost", "jisanjun_jjung", "456pickupsticks.", "jisanjun_orders") or die("error");
+    $requestDate = $_POST["requestDate"];
+    $requestTime = $_POST["requestTime"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $service = $_POST["service"];
+    $inquery = $_POST["inquery"];
+    $date = $_POST["date"];
 
-    if (!$conn) {
-        echo "connection error".mysqli_connect_error();
+    if (empty($requestDate) || empty($requestTime) || empty($name) || empty($email) || empty($phone) || empty($service)) {
+        echo "incomplete";
     } else {
-        $requestDate = $_POST["requestDate"];
-        $requestTime = $_POST["requestTime"];
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $phone = $_POST["phone"];
-        $service = $_POST["service"];
-        $inquery = $_POST["inquery"];
-        $date = $_POST["date"];
+        // send appointment to email
+        $to = 'sandra2938@gmail.com';
+        $subject = 'Message from: '.$name;
+        $txt = "New Appointment from {$name}\n
+                Name: {$name}\n
+                Email: {$email}\n
+                Phone: {$phone}\n
+                Requested Date: {$requestDate}\n
+                Request Time: {$requestTime}\n
+                Service: {$service}\n
+                Message from Customer: {$inquery}\n\n
+                Sent on {$date}";
+        $headers = "From: ".$email;
 
-        if (empty($requestDate) || empty($requestTime) || empty($name) || empty($email) || empty($phone) || empty($service)) {
-            echo "incomplete";
+        if (mail($to, $subject, $txt, $headers)) {
+            echo "pass";
         } else {
-            $query = "insert into appointments(request_date, request_time, name, email, phone, service, inquery, date_posted) values('$requestDate', '$requestTime', '$name', '$email', '$phone', '$service', '$inquery', '$date')";
-
-            $result = mysqli_query($conn, $query);
-
-            if (!$result) {
-                echo "fail";
-            } else {
-                echo "pass";
-            }
+            echo "fail";
         }
+
+        // send appointment to db
+        $conn = new mysqli("localhost", "jisanjun_jjung", "456pickupsticks.", "jisanjun_orders") or die("error");
+
+        $query = "insert into appointments(request_date, request_time, name, email, phone, service, inquery, date_posted) values('$requestDate', '$requestTime', '$name', '$email', '$phone', '$service', '$inquery', '$date')";
+
+        $result = mysqli_query($conn, $query);
     }
 ?>
