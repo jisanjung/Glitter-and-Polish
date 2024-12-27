a001eeb3805cd0adc55f93d185a0ab09f3bcc9c4
Jason Jung <jjjung13@gmail.com>
2024-12-27 14:01:52 -0500

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const cors = require("cors");
+const mongoose = require("mongoose");
 const port = 5000;
 
 // general middleware
@@ -13,4 +15,12 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+// db connect and start server
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    // connect to db
+    console.log("connected to mongodb");
+    // then start server
+    app.listen(port, () => console.log(`server is running on port ${port}`));
+})
+.catch(error => console.log(`mongodb error: ${error}`));
