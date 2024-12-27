c64bac5b805b0688b084d0f94e7b96b24eacd76d
Jason Jung <jjjung13@gmail.com>
2024-12-27 13:39:37 -0500

    setup express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log('hi');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 5000;
+
+// general middleware
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'working'
+    });
+});
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
