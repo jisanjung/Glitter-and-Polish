3e22d0813a3267b388df46ddfce28c07bb4322e5
Jisan Jung <jjjung13@gmail.com>
2026-01-24 11:28:00 -0600

    convert to vite app

--- server/firebase.js
+++ server/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js';
 import { getFirestore, collection, getDocs, addDoc } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js';
 
-const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
-const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
-const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
-const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
-const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
-const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
-const FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID;
+const FIREBASE_API_KEY = import.meta.env.FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = import.meta.env.FIREBASE_AUTH_DOMAIN;
+const FIREBASE_PROJECT_ID = import.meta.env.FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = import.meta.env.FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = import.meta.env.FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = import.meta.env.FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = import.meta.env.FIREBASE_MEASUREMENT_ID;
  
 const firebaseConfig = { 
     apiKey: FIREBASE_API_KEY, 
